4f093de305f658d1f12c8e341869af1f3703ba29
ManGo-WEB <info@mango-web.ru>
2020-06-06 17:35:47 +0800

    Add Bootstrap modal component

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,5 @@
 // Bootstrap //
+//= ../../../node_modules/bootstrap/js/dist/util.js
 //= ../../../node_modules/bootstrap/js/dist/modal.js
-
-// Slick - slider //
-//= ../../../node_modules/slick-carousel/slick/slick.js
-
 
 // Components //
